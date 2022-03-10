c2d6b4442b5ce190eae679fd67dd928b6243326a
r1ky131115 <ricardoaguirre_96@hotmail.com>
2022-03-10 12:13:58 -0300

    dark-mode

---
+++ js/switch.js
@@ -0,0 +1,21 @@
+const btnSwitch = document.querySelector('#switch');
+
+btnSwitch.addEventListener('click', () => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+
+    //Guardar modo en LocalStorage
+    if(document.body.classList.contains('dark')) {
+        localStorage.setItem('dark-mode', 'true');
+    } else {
+        localStorage.setItem('dark-mode', 'false');
+    }
+});
+
+if (localStorage.getItem('dark-mode') === 'true') {
+    document.body.classList.add('dark');
+    btnSwitch.classList.add('active');
+} else {
+    document.body.classList.remove('dark');
+    btnSwitch.classList.remove('active');
+}
